0eb79a6f657438620a03c9399cb68b51d0352c19
Matheus Santos <96088072+devmathx@users.noreply.github.com>
2022-12-19 16:29:04 -0300

    Add pyttsx3

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 from vosk import Model, KaldiRecognizer
-import os
 import pyaudio
+import pyttsx3
+import json
+
+# Síntese de texto
+engine = pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
 
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
-    
-print(rec.FinalResult())
+        result = rec.Result()
+        result = json.loads(result)
+        
+        text = result["text"]
+        
+        print(">>", text)
+        speak(text)
---
+++ tests/tts.py
@@ -0,0 +1,5 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say('Eu vou falar este texto')
+engine.runAndWait()
